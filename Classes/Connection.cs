bc7a20dea34fd3b92585cec6bdfb148b5d2cefbd
Janssen <larsjanssen64@gmail.com>
2017-06-19 11:32:05 +0200

    first commit

---
+++ Classes/Connection.cs
@@ -0,0 +1,37 @@
+﻿using LivePerformance.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LivePerformance.Classes
+{
+    public class Connection : IConnection
+    {
+        public SqlConnection databaseConnection;
+        public void Connect()
+        {
+            databaseConnection = new SqlConnection("Server=mssql.fhict.local;Database=dbi366017;User Id=dbi366017;Password=larsjanssen;");
+            try
+            {
+                databaseConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void disConnect()
+        {
+            databaseConnection.Close();
+        }
+
+        public SqlConnection getConnection()
+        {
+            return databaseConnection;
+        }
+    }
+    
+}
---
+++ Form1.cs
@@ -0,0 +1,44 @@
+﻿using LivePerformance.Classes;
+using LivePerformance.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LivePerformance
+{
+    public partial class Form1 : Form
+    {
+        private IConnection connection;
+
+        public Form1()
+        {
+            InitializeComponent();
+            connection = new Connection();
+            testConnection();
+        }
+
+        public void testConnection()
+        {
+            connection.Connect();
+            SqlCommand sqlCommand = new SqlCommand("select * from test", connection.getConnection());
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    MessageBox.Show(reader["test"].ToString());
+                }
+            }
+
+            connection.disConnect();
+        }
+    }
+}
